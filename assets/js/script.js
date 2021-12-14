da4c5a6a3ba239b566d44742c8919985603a7f8f
CTemp3 <92496739+CTemp3@users.noreply.github.com>
2021-12-14 10:32:46 -0700

    pulling real content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
-$("search-button").on("click", function() {
-    console.log("This button click event works!");
+// search history saving
+$("#search-button").on("click", function() {
+    // console.log("This button click event works!");
+    var searchtext = $(".superhero-select").val();
+    console.log(searchtext);
 });
