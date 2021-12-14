d9dbc3b5dccca641d8ce9c94be6975cbe55f688b
Ddorto1 <dallasdorton@yahoo.com>
2021-12-13 21:29:32 -0700

    push functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 var SuperheroName= document.getElementById("hero-name");
 var About= document.getElementById("description");
 var Comic= document.getElementById("comics");
-var Search=document.getElementsByClassName("Search");
+var Search=document.getElementById("Search");
 var Luke= "https://swapi.dev/api/people/1";
 var C3PO= "https://swapi.dev/api/people/2";
 var R2D2= "https://swapi.dev/api/people/3";
 var darthVader= "https://swapi.dev/api/people/4";
 var Leia= "https://swapi.dev/api/people/5";
 
-
+var skywalker= document.getElementById('Skywalker');
+var friend= document.getElementById('C3P0');
+var r2=document.getElementById('R2-D2');
+var darth=document.getElementById('Darth');
+var princess=document.getElementById('Princess Leia');
 
 fetch(Luke).then(function(response){
     //request successful
@@ -18,6 +22,11 @@ fetch(Luke).then(function(response){
         });
     }
     });
+function pushSkywalker(){
+      About.append (Luke.name);
+}
+
+skywalker.addEventListener("onClick", pushSkywalker);
 
 fetch(C3PO).then(function(response){
     //request successful
@@ -27,6 +36,11 @@ fetch(C3PO).then(function(response){
         });
         }
         });
+    function pushC3PO(){
+        About.append (C3PO.name);
+      }
+      
+      friend.addEventListener("onClick", pushC3PO);
 
 fetch(R2D2).then(function(response){
     //request successful
@@ -36,8 +50,14 @@ fetch(R2D2).then(function(response){
     });
     }
     });
-    
-fetch(darthVader).then(function(response){
+ 
+    function pushR2D2(){
+        About.append (R2D2.name);
+      }
+      
+      r2.addEventListener("onClick", pushR2D2);
+
+  fetch(darthVader).then(function(response){
     //request successful
     if (response.ok){
         response.json().then(function(data){
@@ -45,7 +65,13 @@ fetch(darthVader).then(function(response){
     });
     }
     }); 
-    
+  
+    function pushDarth(){
+        About.append (darthVader.name);
+      }
+      
+      darth.addEventListener("onClick", pushDarth);
+
 fetch(Leia).then(function(response){
     //request successful
     if (response.ok){
@@ -54,12 +80,12 @@ fetch(Leia).then(function(response){
     });
     }
     }); 
-function pull (data){
-    About.p.append(data.description);
-    Comic.p.append(data.comics);
-}
 
-// document.getElementByClassName("Search").addEventListener("onClick", pull);
+    function pushLeia(){
+        About.append (Leia.name);
+      }
+      
+      princess.addEventListener("onClick", pushLeia);
    
 
 var searchHistory = {};
